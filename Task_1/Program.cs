dbc4f82d56b592a2bf210e88b5130cdd3356a651
Lazarev Konstantin <klik@example.com>
2022-09-10 20:26:49 +0300

    тип double

---
+++ Task_1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте двумерный массив размером m×n, 
+// заполненный случайными вещественными 
+// (тип double) числами.
+
+int n = 3, m = 3;
+
+Random rnd = new Random();
+
+double[,] array = new double[n, m];
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        array[i, j] = rnd.NextDouble() * 100;
+        Console.Write("{0:F2} ", array[i,j]);
+    }
+    Console.WriteLine();
+}
