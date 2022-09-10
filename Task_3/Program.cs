496e00c9f932fe257d61d7be41a6a3922e1a6435
Lazarev Konstantin <klik@example.com>
2022-09-10 20:29:06 +0300

    Среднее арифметическое

---
+++ Task_3/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+int n = 3, m = 4; 
+double avg;
+int[,] array = new int[n, m];
+
+
+Random rand = new Random();
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        array[i, j] = rand.Next(10);
+        Console.Write("{0} ", array[i, j]);
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+for (int i = 0; i < m; i++)
+{
+    int sum = 0;
+
+    for (int j = 0; j < n; j++)
+    {
+        sum += array[j, i];
+    }
+    avg = (double) sum / n;
+    Console.Write("{0:f2} ", avg);
+}
